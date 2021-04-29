8099c075dc587cff25d29925d0d8e41e8afdfb13
Tijuan Brown <brownti159@gmail.com>
2021-04-29 17:44:49 -0400

    Got code to show underscores for letters in word

---
+++ wheel_game.py
@@ -0,0 +1,45 @@
+#import random
+#import words.txt
+
+
+
+#ask user for mode "Easy", "Normal", or "Hard"
+
+
+#select word
+#function for selecting word
+#def get_word()
+
+word = "GOOGLE"
+
+#display the number of letters in word
+mystery_word = ' _ ' * len(word)
+print(mystery_word)
+
+
+# def show_word(word):
+#     for i in range(0, len(word)):
+#         if ord(word[i]) != 32:
+#             new_word = word.replace(word[i], '_')
+#     print(new_word)
+
+
+# For character in word:
+#     if character in guesses:
+#         print("_"character "_")
+#     else:
+#         print("_")
+
+#function to play game
+#def play_game():
+
+#ask for user input /print statement to guess letter
+
+#user inputs guess
+
+#return whether the letter is in the word or not
+
+#if true: return the word with the remaining letters to be guessed
+
+#if false return "incorrect, guess again.
+# show_word(word)
