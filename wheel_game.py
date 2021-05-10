d2ad8e083bec9f2a912f2edf2134aaf6fa0e064d
Tijuan Brown <brownti159@gmail.com>
2021-05-10 12:42:13 -0400

    word pulls from file list

--- wheel_game.py
+++ wheel_game.py
@@ -1,51 +1,75 @@
-attemps = 8
 
-word = "GOOGLE"
-print("Welcome to Wheel of Fortune!")
+import random
 
-#display the number of letters in word
-def show_mystery_word(word):
-    return ' _ ' * len(word)
-print(show_mystery_word(word))
-print("")
 
-#https://stackoverflow.com/questions/60617366/how-to-reveal-a-specific-letter-in-a-string-for-python
-def attempt_guess():
-    total_guesses= 8
-    guesses = 0
-    gameboard = show_mystery_word(word)
-    print(gameboard)
-    while guesses < total_guesses:
-        guess = input("Please guess a letter: ")
-        if guess.upper() not in word:
-            guess += 1
-            print("Incorrect, guesses remaining:", total_guesses - guesses, "Try again.")
-        else: 
-            print("Correct!")
-            new_gameboard = ''
-            for w,l in zip(word, gameboard):
-                if guess.upper() == w:
-                    new_gameboard += guess.upper()
-                else:
-                    new_gameboard += l
-            gameboard= new_gameboard
-            print(gameboard)
-            if not " _ " in gameboard:
-                print("Congratulations, You win! Would you like to play again?")
-attempt_guess()
-print()
+class Word:
+    def __init__(self):
+        with open('words.txt', 'r') as file:
+            word_list = file.read()
+        word_list = word_list.splitlines()
+
+        self.characters = random.choice(word_list)
+        self.word_length = len(self.characters)
+        
+
+        # use word_length to build a function for difficulty: "easy", "normal" or "hard" levels
+        print(self.characters)    
+        # return word.upper()
+    # def sort_word(self):
+    #     easy_list =[]
+    #     normal_list=[]
+    #     hard_list=[]
+    #     for word in word_list:
+    #         if word_length < 6:
+    #             append.easy_list
+    #         elif word_length == 6 or 7:
+    #             apppend.normal_list 
+    #         else:
+    #             append.hard_list 
+                
+              
+    def show_mystery_word(self):
+        gameboard = '_ ' * self.word_length
+        print(gameboard)
+        return gameboard
     
 
-# guess = input("Please guess a letter:  ").upper()
+
+#https://stackoverflow.com/questions/60617366/how-to-reveal-a-specific-letter-in-a-string-for-python
+
+class Game:
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+
+    def attempt_guess(self):
+        total_guesses= 8
+        guesses = 0
+        word = Word()
+        print(word.characters)
+        gameboard = word.show_mystery_word()
+        gameboard = list(gameboard)
+        while guesses < total_guesses:
+            guess = input("Please guess a letter: ")
+            
+            if guess.lower() not in word.characters:
+                guesses += 1
+                print("Incorrect, guesses remaining:", total_guesses - guesses, "Try again.")
+            else: 
+                print("Correct!")
+            
+                for i in range(len(word.characters)):
+                    print(i)
+                    if guess.lower() == word.characters[i]:
+                        gameboard[i * 2] = guess.upper()
+                
+                  
+                print("".join(gameboard))
+            print(guesses)
+            if not "_" in gameboard:
+                print("Congratulations, You win! Would you like to play again?")
 
 
+# difficulty = input("Do you want to play difficulty level Easy, Normal, or Hard?" )
+new_game = Game('easy')
 
-# if guess not in word:
-#     print(guess "is not in the word.")
-#     tries -= 1
-#     guessed_letters.append(guess)
-# else:
-#     print("Good job!")
-#     tries -= 1
-#     guessed_letters.append(guess)    
-
+new_game.attempt_guess()
--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,30 +1,36 @@
-import random
-import list of random words txt.failed
-
-
-
+word = "SPAM"
 print("Welcome to Wheel of Fortune!")
 
-words = ['cats', 'dogs', 'baby', 'duck', 'tree', 'bird', 'code']
+# display the number of letters in word
+def show_mystery_word(word):
+    return '_ ' * len(word)
+    print(show_mystery_word(word))
+    print("")
 
-word = random.choice(words)
-
-print("Guess a letter.")
-
-guesses = ' '
-turns = 8
-
-while turns > 0:
-    failed = 0
-    for letter in word:
-        if letter in guesses:
-            print(letter)
-        else:
-            print(" __ ")
-            failed += 1
-
-    if failed == 0:
-        print("You win!")
-        print("The word is: ", word)
-        break
+#https://stackoverflow.com/questions/60617366/how-to-reveal-a-specific-letter-in-a-string-for-python
+def attempt_guess():
+    total_guesses= 8
+    guesses = 0
+    gameboard= show_mystery_word(word)
+    print(gameboard)
+    gameboard = list(gameboard)
+    while guesses < total_guesses:
+        guess = input("Please guess a letter: ")
+        if guess.upper() not in word:
+            guesses += 1
+            print("Incorrect, guesses remaining:", total_guesses - guesses, "Try again.")
+        else: 
+            print("Correct!")
+           
+            for letter in word:
+                if guess.upper() == letter:
+                    gameboard[word.index(letter) * 2] = guess.upper()
+                
+               
+            print("".join(gameboard))
+            print(guesses)
+            if not "_" in gameboard:
+                print("Congratulations, You win! Would you like to play again?")
+attempt_guess()
+print()
                 
